54f5a2036950992f7c6e0e851043b04f6734d8da
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2025-05-12 22:24:48 +0630

    Desktop

--- keyword_only_args.py
+++
@@ -1,7 +0,0 @@
-def details(name, address, *, age):
-    print(f"Name is {name}")
-    print(f"Address is {address}")
-    print(f"Age is {age}")
-
-details("Aung Aung","Yangon", age=34)
-
--- recursion.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1 # base case
-    else:
-        return n * factorial(n - 1) # recursive case
-
-print(factorial(5))  # Output: 120
-
