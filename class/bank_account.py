5d0a0a2b4b1c289bfea5373c5d74aa0d80e9674b
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2025-05-13 04:29:17 +0630

    Desktop

--- class/bank_account.py
+++ class/bank_account.py
@@ -13,6 +13,7 @@ class Bank:
             print(f"ငွေပမာဏ {w_amount} ကျပ် ထုတ်ယူပါသည်။\n")
         else:
             print("Insufficient balance!")
+
     def get_balance(self):
         return self.balance
 
@@ -23,27 +24,27 @@ p1 = Bank(name,initial_balance)
 print(f"လက်ရှိလက်ကျန်ငွေမှာ {p1.get_balance()} ကျပ်ဖြစ်ပါသည်။\n")
 
 while True:
-    question = int(input("ငွေထပ်မံထည့်သွင်းလိုပါက 1 ကိုနှိပ်ပါ။\n"
+    question = input("ငွေထပ်မံထည့်သွင်းလိုပါက 1 ကိုနှိပ်ပါ။\n"
                          "ငွေထုတ်ယူလိုပါက 2 ကိုနှိပ်ပါ။\n"
                          "ငွေလက်ကျန် စစ်ဆေးလိုပါက 3 ကိုနှိပ်ပါ။\n"
                          "ပရိုဂရမ်မှ ထွက်လိုပါက 0 ကိုနှိပ်ပါ။\n"
-                         "ရွေးချယ်ပါ။ :\n"))
-    if question == 1:
+                         "ရွေးချယ်ပါ။ :\n")
+    if question == "1":
         deposit = float(input("ထပ်မံထည့်သွင်းမည့် ပမာဏ = "))
         p1.deposit(deposit)
 
 
-    elif question == 2:
+    elif question == "2":
         withdraw = float(input("ထုတ်ယူမည့် ပမာဏ = "))
         p1.withdraw(withdraw)
 
-    elif question == 3:
+    elif question == "3":
         print(f"လက်ရှိလက်ကျန်ငွေမှာ {p1.get_balance()} ကျပ်ဖြစ်ပါသည်။\n")
 
-    elif question == 0:
+    elif question == "0":
         print("ပရိုဂရမ်မှထွက်ပါသည်။ ကျေးဇူးတင်ရှိပါသည်။")
         break
 
     else:
-        print("1 (သို့) 2 (သို့) 3 ကိုသာရွေးချယ်ပါ။\n")
+        print("1 (သို့) 2 (သို့) 3 (သို့) 0 ကိုသာရွေးချယ်ပါ။\n")
 
