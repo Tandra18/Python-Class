cdf0c64ea1b5c39b21540bc690277352bbba2a71
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2025-05-16 03:03:57 +0630

    desktop

---
+++ iterators/class_iteration.py
@@ -0,0 +1,18 @@
+class Greeting:
+    def __init__(self, greet):
+        self.greet = greet
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.greet):
+            raise StopIteration
+        char = self.greet[self.index]
+        self.index += 1
+        return char
+
+output = Greeting("hello")
+for x in output:
+    print(x)
---
+++ iterators/iteration.py
@@ -0,0 +1,32 @@
+# greeting = "Hello"
+# # create iterator object
+# my_iter = iter(greeting)
+#
+# print(next(my_iter))#H
+# print(next(my_iter))#e
+# print(next(my_iter))#l
+# print(next(my_iter))#l
+# print(next(my_iter))#o
+#
+#
+
+# fruits = ("apple","banana","cherry")
+# for x in fruits:
+#     print(x)
+#
+
+# greeting = "hello"
+# for x in greeting:
+#     print(x)
+
+greeting = "hello"
+it = iter(greeting)
+while True:
+    try:
+        x = next(it)
+        print(x)
+    except StopIteration:
+        break
+
+
+
