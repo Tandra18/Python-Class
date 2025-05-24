60010c4271373b0f36f8cb1e76a4f91e4b68aed5
Zaw Ye Naung <topgzawyenaung@gmail.com>
2025-05-24 18:34:28 +0630

    laptop

--- file_handling/open_file.py
+++ file_handling/open_file.py
@@ -2,17 +2,47 @@
 # content = file.read()
 # print(content)
 # file.close()
+from dataclasses import field
 
 # with open('D:\details.txt','r') as file:
 #     content = file.read()
 #     print(content)
 
-with open('D:\details.txt','w') as file:
-    file.write("Zaw Ye Naung is so handsome!")
 
-with open('D:\details.txt','a') as file:
-    file.write("He also play football.")
 
-with open('D:\details.txt','r') as file:
-    content = file.read()
-    print(content)
+# with open('D:\details.txt','w') as file:
+#     file.write("Zaw Ye Naung is so handsome!")
+#
+# with open('D:\details.txt','a') as file:
+#     file.write("He also play football.")
+#
+# with open('D:\details.txt','r') as file:
+#     content = file.read()
+#     print(content)
+
+# try:
+#     with open(r'D:\fruits.txt','x') as file:
+#         file.write("apple, banana, orange")
+#
+# except FileExistsError:
+#     print("File name already exists!")
+
+
+# with open(r'D:\fruits.txt','r+') as file:
+#     content = file.read()
+#     print("Original : ",content)
+#
+#     file.write(" are my favorite!")
+#     file.seek(0)
+#     print("Updated : ",file.read())
+
+# with open(r'D:\fruits.txt','w+') as file:
+#     file.write("I don't like other fruits!")
+#     file.seek(0)
+#     print(file.read())
+
+
+with open(r'D:\fruits.txt','a+') as file:
+    file.write("I like apple, banana and orange!")
+    file.seek(0)
+    print(file.read())
