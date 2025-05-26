5ab0908666e28f32e21da86d0eeb5807ca2a0d23
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2025-05-27 03:46:36 +0630

    desktop

---
+++ graphical_user_interface/check_button.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Check Button")
+root.geometry("300x300")
+
+def show_answer():
+    result = []
+    if var1.get():result.append("Option A")
+    if var2.get():result.append("Option B")
+    label.config(text="Selected: " + ", ".join(result))
+
+var1 = tk.BooleanVar()
+var2 = tk.BooleanVar()
+
+tk.Checkbutton(root,text="Option A",variable=var1).pack(padx=10)
+tk.Checkbutton(root,text="Option B",variable=var2).pack(padx=10)
+
+tk.Button(root,text="Submit",command=show_answer).pack()
+label = tk.Label(root,text="Choose!")
+label.pack(pady=10)
+
+root.mainloop()
---
+++ graphical_user_interface/multiple_line_text_input.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Multiple line input")
+root.geometry("400x400")
+
+def get_text():
+    content = text.get("1.0",tk.END).strip()
+    label.config(text=f"Text entered:\n{content}")
+
+
+text = tk.Text(root,height=5,width=40)
+text.pack(pady=10)
+
+button = tk.Button(root,text="Get Text",command=get_text)
+button.pack()
+
+label = tk.Label(root,text="",justify="left")
+label.pack(pady=10)
+
+root.mainloop()
